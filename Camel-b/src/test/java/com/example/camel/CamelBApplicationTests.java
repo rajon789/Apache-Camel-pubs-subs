3a3dbecb5184c3b9c7e0efbc1191b3e01fa80304
MH Rajon <rajon789@gmail.com>
2022-09-26 15:57:31 +0600

    Add files via upload

---
+++ Camel-b/src/test/java/com/example/camel/CamelBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
